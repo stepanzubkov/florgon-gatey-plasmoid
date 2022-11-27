910542e4d9da9a24d49e426cc1f683d23f504d46
Stepan Zubkov <zubkovbackend@gmail.com>
2022-11-27 21:33:47 +0300

    Request for events after choosing project in config, send custom User-Agent with request

--- plasmoid/contents/ui/events.js
+++ plasmoid/contents/ui/events.js
@@ -2,11 +2,11 @@ function requestForEvents(accessToken, project_id, callback) {
     if (!project_id) {
         return [];
     }
-   
     
     var request = new XMLHttpRequest();
     request.open("GET", `https://api-gatey.florgon.space/v1/project.getEvents?project_id=${project_id}&limit=20&offset=0`);
     request.setRequestHeader("Authorization", accessToken);
+    request.setRequestHeader("User-Agent", "florgon-gatey-plasmoid (github.com/stepan-zubkov/florgon-gatey-plasmoid)");
 
     request.onreadystatechange = function () { callback(request); }
 
